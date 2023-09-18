c5e811d1cfc34e1d3c6693e64b7157eec9de3699
Shadrack kipkorir <shadybett540@gmail.com>
2023-09-18 22:38:19 +0300

    added some features

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+typeof "const currentUser = 'Grace Hopper';";
+"const currentUser = 'Grace Hopper';".length;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
